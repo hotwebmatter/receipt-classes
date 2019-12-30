d193e287c390d5eaef122d8f3a7c780b3041b2fe
Matt Obert on Windows <matt@hotwebmatter.com>
2019-12-30 09:25:27 -0500

    Use static System.Console

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 
 namespace receipt_classes
 {
@@ -6,7 +7,7 @@ namespace receipt_classes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            WriteLine("Hello World!");
         }
     }
 }
